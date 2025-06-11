14a55089f2be5bee35fd68dd47fc7b7a750ad33f
drema3-4 <drema3-4@yandex.ru>
2025-06-11 16:56:56 +0300

    Закончил редактирование практической части (без итогов) по парсингу

--- work_text/code/parcing/get_link_and_summary.py
+++ work_text/code/parcing/get_link_and_summary.py
@@ -1,22 +1,14 @@
-# получение html кода страницы из файла
 with open(page_file_name, encoding="utf-8") as file:
         src = file.read()
-# преобразование html кода в классы
 soup = BeautifulSoup(src, "lxml")
-# переход к содержимому новости, которое находится
-# в теге div с классом post
 news = soup.find("div", class_="post")
 try:
-    # получение текста ссылки из соответствующего тега
     link = news.find("h2", class_="first_child").find("a").get("href")
-    # не все ссылки в теге сохранены полностью, данный
-    # код добавляет обрезанную часть
     if not link.startswith("https://"):
         link = 'https://www.hse.ru' + link
 except:
     link = ""
 try:
-    # получение краткого описания новости из соответствующего тега
     news_short_content = news.find("p", class_="first_child").find_next_sibling("p").text.strip()
 except:
     news_short_content = ""
--- work_text/code/parcing/get_page_function.py
+++ work_text/code/parcing/get_page_function.py
@@ -1,6 +1,4 @@
 def __getPage__(url: str, file_name: str) -> None:
-    # получение html кода страницы с помощью библиотеки requests
     r = requests.get(url=url)
-    # сохранение полученного кода в текстовый файл
     with open(file_name, "w", encoding="utf-8") as file:
         file.write(r.text)
